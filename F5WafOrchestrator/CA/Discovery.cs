4afcc627b4b691e6d555edbc987ac31cb6c0da5e
Lee Fine <50836957+leefine02@users.noreply.github.com>
2024-06-17 19:22:37 +0000

    Ab#52646 (#2)
    * Initial public release

--- F5WafOrchestrator/CA/Discovery.cs
+++ F5WafOrchestrator/CA/Discovery.cs
@@ -1,3 +1,17 @@
+// Copyright 2024 Keyfactor
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 using Keyfactor.Extensions.Orchestrator.F5WafOrchestrator.Client;
 using Keyfactor.Logging;
 using Keyfactor.Orchestrators.Common.Enums;
@@ -6,47 +20,33 @@ using Microsoft.Extensions.Logging;
 
 namespace Keyfactor.Extensions.Orchestrator.F5WafOrchestrator.CA;
 
-[Job("Discovery")]
-public class Discovery : Job<Discovery>, IDiscoveryJobExtension
+public class Discovery : Job, IDiscoveryJobExtension
 {
     ILogger _logger = LogHandler.GetClassLogger<Discovery>();
-        
-        public JobResult ProcessJob(DiscoveryJobConfiguration config, SubmitDiscoveryUpdate cb)
-        {
-            _logger.LogDebug("Beginning F5 Distributed Cloud Discovery Job");
-        
-            var result = new JobResult
-            {
-                Result = OrchestratorJobStatusJobResult.Failure,
-                JobHistoryId = config.JobHistoryId
-            };
-        
-            try
-            {
-                F5Client = new F5WafClient(config.ClientMachine, config.ServerPassword);
-            } catch (Exception ex)
-            {
-                _logger.LogError(ex, $"Could not connect to F5 Client" + ex.Message);
-                return result;
-            }
-            
-            List<string> namespaces;
-            
-            try
-            {
-                namespaces = F5Client.DiscoverNamespacesforCaStoreType();
-                _logger.LogDebug($"Found {namespaces.Count()} namespaces in {config.ClientMachine}");
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, $"Error getting F5 namespaces from {config.ClientMachine}:\n" + ex.Message);
-                result.FailureMessage = $"Error getting F5 namespaces from {config.ClientMachine}:\n" + ex.Message;
-                return result;
-            }
-            
-            cb.Invoke(namespaces);
 
-            result.Result = OrchestratorJobStatusJobResult.Success;
-            return result;
+    public JobResult ProcessJob(DiscoveryJobConfiguration config, SubmitDiscoveryUpdate cb)
+    {
+        _logger.LogDebug($"Begin {config.Capability} for job id {config.JobId}...");
+        _logger.LogDebug($"Server: {config.ClientMachine}");
+
+        List<string> namespaces;
+
+        try
+        {
+            F5Client = new F5WafClient(config.ClientMachine, config.ServerPassword);
+
+            namespaces = F5Client.DiscoverNamespacesforStoreType("ca-");
+            _logger.LogDebug($"Found {namespaces.Count()} namespaces in {config.ClientMachine}");
         }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Exception for {config.Capability}: {F5WAFException.FlattenExceptionMessages(ex, string.Empty)} for job id {config.JobId}");
+            return new JobResult() { Result = OrchestratorJobStatusJobResult.Failure, JobHistoryId = config.JobHistoryId, FailureMessage = F5WAFException.FlattenExceptionMessages(ex, $"Server {config.ClientMachine}:") };
+        }
+
+        cb.Invoke(namespaces);
+
+        _logger.LogDebug($"...End {config.Capability} job for job id {config.JobId}");
+        return new JobResult() { Result = OrchestratorJobStatusJobResult.Success, JobHistoryId = config.JobHistoryId };
+    }
 }
